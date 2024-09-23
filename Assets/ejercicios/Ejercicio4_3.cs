5f37da61408955c93e3043fe6976f7ad74bd5a92
daniiela08 <danielabarrosoruiz@gmail.com>
2024-09-23 20:40:24 +0200

    mejora

--- Assets/ejercicios/Ejercicio4_3.cs
+++ Assets/ejercicios/Ejercicio4_3.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Ejercicio4_3 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float cantidad;
+    [SerializeField] string divisa;
+    string cambio;
     void Start()
     {
-        
-    }
+        switch (divisa)
+        {
+            case "Dolar":
+            case "dolar":
+            case "Dólar":
+            case "dólar":
+            case "Dolares":
+            case "dolares":
+            case "Dólares":
+            case "dólares":
+            case "$":
+                cambio = "€";
 
-    // Update is called once per frame
-    void Update()
+                break;
+            case "Euro":
+            case "euro":
+            case "Euros":
+            case "euros":
+            case "€":
+                cambio = "$";
+
+                break;
+            default:
+                Debug.Log("No es una divisa válida");
+                break;
+        }
+        float resultado = Convertir(cantidad, cambio);
+        Debug.Log("El cambio a realizar queda en " + resultado + " " + cambio + ".");
+
+    }
+    float Convertir(float cantidad, string moneda)
     {
-        
+        float calculo;
+        if (moneda == "€")
+        {
+            calculo = cantidad * 0.9f;
+        }
+        else
+        {
+            calculo = cantidad * 1.1111111f;
+        }
+        return calculo;
     }
 }
